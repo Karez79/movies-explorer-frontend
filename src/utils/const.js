221ae5cca91de64e510db2aef66e89ee62434e0b
Ivan Kozlov <karez78@yandex.ru>
2023-09-21 18:50:58 +0200

    первая проверка level 3

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,3 @@
-export const MAIN_URL = "https://api.ivan.nomoredomainsicu.ru,";
+export const MAIN_URL = "https://api.ivan.nomoredomainsicu.ru";
 
 export const MOVIES_URL = "https://api.nomoreparties.co";
